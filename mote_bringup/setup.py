b09d86361ee8be1c21d4610ad4832b0788c1c757
Michael Johnson <mjohnson459@gmail.com>
2026-05-25 12:07:38 +0100

    Change emails to clach.dev

--- mote_bringup/setup.py
+++ mote_bringup/setup.py
@@ -18,7 +18,7 @@ setup(
     install_requires=["setuptools"],
     zip_safe=True,
     maintainer="Michael Johnson",
-    maintainer_email="mjohnson@augereai.com",
+    maintainer_email="michael@clach.dev",
     description="Launch files for the mote",
     license="MIT",
     entry_points={
